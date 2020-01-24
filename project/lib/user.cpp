6851bb1ee5104f16441d1978b4f3c32f5a2799fb
Travis Booth <travswim@gmail.com>
2020-01-24 14:56:47 -0800

    Added methods destroyGame() and kickPlayer()

--- project/lib/user.cpp
+++ project/lib/user.cpp
@@ -5,7 +5,6 @@
 using rolls::User;
 
 User::User(std::string username)
-
     : userName{username},
      userType{UserType::GAMER}
 {
@@ -26,6 +25,36 @@ User::createGame(/* WILL NEED TO PASS SOME FORM OF JSON OR STRING */)
     userType = UserType::OWNER;
     gamesCreated.push_back("gameName");
     // Do stuff to create game
+}
+
+bool
+User::destroyGame(std::string gameName)
+{
+    if (User::userType ==  User::UserType::OWNER)
+    {
+        // Kick gamers out
+        // Destroy game "gameName"
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+bool
+User::kickPlayer(std::string username)
+{
+    if (User::userType == User::UserType::OWNER)
+    {
+        // Kick gamers with "userName" out
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 void
