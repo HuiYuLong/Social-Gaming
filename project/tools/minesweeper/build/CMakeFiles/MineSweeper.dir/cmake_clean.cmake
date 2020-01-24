9aebb93cd7638308b85ad2a8512f61ef77932310
namt <nam_tang@sfu.ca>
2020-01-23 23:06:13 -0800

    Fixes #2

---
+++ project/tools/minesweeper/build/CMakeFiles/MineSweeper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MineSweeper.dir/MineSweeper.cpp.o"
+  "MineSweeper"
+  "MineSweeper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MineSweeper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- project/tools/minesweeper/minesweeper.cpp
+++ project/tools/minesweeper/minesweeper.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
+class Cell {
+public:
+	Cell(int number, bool isHidden): number(number), isHidden(isHidden){}
+	int getNumber() const { return number;}
+	bool getIsHidden() const { return isHidden;}
+	void setNumber(const int& number) {this->number = number;}
+	void setIsHidden(const bool& isHidden) {this->isHidden = isHidden;}
+
+private:
+	int number; // number can be -1,0,1,2,3,4,5,6,7,8. -1 means the cell has a bomb.
+	bool isHidden; // Is the cell selected by the user
+};
+ 
 int main() {
-	cout << "Hello World" << endl;
+	
 	return 0;
 }
