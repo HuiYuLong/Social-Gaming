f3db9dea09c441182cbd99b1719a29554b8ac690
hylong <hylong@sfu.ca>
2020-02-04 23:50:12 -0800

    authentication modified

--- lib/authentication/test.h
+++ lib/authentication/test.h
@@ -13,7 +13,7 @@ namespace rolls
 class User {
 public:
     // Define User Types. One of 'GAMER', 'OWNER', or 'MAINTAINER'
-    enum class UserType : uint8_t {
+    enum class UserType : uint8_t{
         GAMER,      // Someone who joins a game --> DEFAULT for now
         OWNER,      // Someone who creates a game
         MAINTAINER  // Server maintainer --> Not used yet
@@ -21,10 +21,11 @@ public:
 private:
     std::string userName;
     std::vector<std::string> gamesPlayed;
-    std::vector<std::string> gamesCreated;
+    // std::vector<std::string> gamesCreated;
     UserType userType;
 
 public:
+    std::vector<std::string> gamesCreated;
     // Constructor
     User(std::string username);
 
@@ -35,7 +36,7 @@ public:
     void joinGame(std::string token);
 
     // Create a game
-    void createGame(/* WILL NEED TO PASS SOME FORM OF JSON OR STRING */);
+    void createGame(std::vector<std::string>& gameslist/* WILL NEED TO PASS SOME FORM OF JSON OR STRING */);
 
     // Leave a game
     void leaveGame();
@@ -45,6 +46,8 @@ public:
 
     // List the game a user has created
     void listGamesCreated(const std::vector<std::string>& gamesCreated);
+
+    UserType getUserType() { return userType; };
     
 };  // class Users
 }   // namespace rolls
--- lib/authentication/user.cpp
+++ lib/authentication/user.cpp
@@ -19,72 +19,52 @@ User::joinGame(std::string token)
     userType = UserType::GAMER;
     gamesPlayed.push_back("gameName");
     // Do stuff to join game
-
-    // look for matches in the gameslist
-    for (const auto& game: gameslist)
-    {
-        // if the Game exist, then join
-        int count = token.compare(game);
-        if(count == 0)
-            // connect to the GameServer
-        else
-            cout << "Game does not exist" << endl;
-    }
-} 
 }
 
 void
-User::createGame(/* WILL NEED TO PASS SOME FORM OF JSON OR STRING */)
+User::createGame(std::vector<std::string>& gamesCreated/* WILL NEED TO PASS SOME FORM OF JSON OR STRING */)
 {
     userType = UserType::OWNER;
     // Do stuff to create game
     
     // Assume passed a string called NameId
-    for (const auto& game: gameslist)
-    {
-        // if the Game Id is unique, then create a new game
-        int count = NameId.compare(game);
-        if(count != 0)      
-        {
-            gamesCreated.push_back("gameName");
-            joinGame(NameId);
-        }
-        else
-            cout << "Game already exists" << endl;
-    }
+    // if(!inList(NameId, gameslist)) {
+        // Create a new game (need a Game class)
+        gamesCreated.push_back("gameName");
+    // }
 
 
 }
 
-bool
-User::destroyGame(std::string gameName)
-{
-    if (User::userType ==  User::UserType::OWNER)
-    {
-        // Kick gamers out
-        // Destroy game "gameName"
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+// bool
+// User::destroyGame(std::string gameName)
+// {
+//     if (this->getUserType() == OWNER)
+//     {
+//         // Kick gamers out
+//         // Destroy game "gameName"
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
     
-}
+// }
 
-bool
-User::kickPlayer(std::string username)
-{
-    if (User::userType == User::UserType::OWNER)
-    {
-        // Kick gamers with "userName" out
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+// bool
+// User::kickPlayer(std::string username)
+// {
+//     if (this->getUserType() == OWNER)
+//     {
+//         // Kick gamers with "userName" out
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
 
 void
 User::leaveGame(/* WILL NEED TO PASS SOME FORM OF JSON OR STRING */)
@@ -113,14 +93,25 @@ User::listGamesCreated(const std::vector<std::string>& gamesCreated)
     std::cout << std::endl;
 }
 
-
+bool 
+inList(std::string GameId, const std::vector<std::string>& gamesCreated) {
+    for(auto& game:gamesCreated) {
+        if(GameId.std::string::compare(game) == 0) {
+            std::cout << "game already exists" << std::endl;
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int main() {
     std::string username = "Sophia";
-    // User User(UserType::GAMER);
+    User User(username);
 
-    // newUser.createGame();
-    // newUser.listGamesCreated();
+    std::vector<std::string> gamesCreated;
+    User.createGame(gamesCreated);
+    std::cout << "Games that are created: ";
+    User.listGamesCreated(gamesCreated);
 
     return 0;
 
