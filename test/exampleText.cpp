a3dd1bb402329b92d99c08eb9a3e2a1ff08eaff8
junhos <junhos@sfu.ca>
2020-03-20 15:36:10 -0700

    Pseudoserver added with send() method that will push Msg to a vector, dunno if we need receive() for testing rules, might need it in the future if we decide to test server functionality

--- test/exampleText.cpp
+++ test/exampleText.cpp
@@ -1,6 +1,40 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "Server.h"
+#include "translator.h"
 
+#include <nlohmann/json.hpp>
+#include <vector>
+
+using networking::Server;
+using networking::Connection;
+using networking::Message;
+using networking::ConnectionHash;
+
+using json = nlohmann::json;
+
+void onConnect();
+void onDisconnect();
+
+class PseudoServer : public Server {
+public:
+	void send(const Message& message) {
+		this->messages.push_back(message);
+	}
+private:
+	std::vector<Message> messages;
+};
+
+class MockAddRule : public AddRule {
+public:
+	MOCK_METHOD2(run, void(Server& server, GameState& state));
+};
+
+
+TEST(MyDumbTest, TestFalse) {
+    EXPECT_FALSE(3*2 == 5);
+    ASSERT_TRUE(true);
+}
 
 
 // Test Example
@@ -10,7 +44,8 @@
 
 // EXPECT_*() will continue to run the other tests even if that test fails
 // ASSERT_*() will stop other tests from running
-TEST(MyDumbTest, TestFalse) {
-    EXPECT_FALSE(3*2 == 5);
-    ASSERT_TRUE(true);
-}
+
+// we'll need a test.json
+// build a GameState and a Server
+
+// pseudoserver with a vector of messages 
