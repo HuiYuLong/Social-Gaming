f98ba70bba7fee0be98dfe23be1b5163739c64a9
junhos <junhos@sfu.ca>
2020-03-20 17:18:50 -0700

    init code sorta working

--- test/exampleText.cpp
+++ test/exampleText.cpp
@@ -25,14 +25,14 @@ private:
 };
 
 
-void GlobalMessageRule::run(Server& server, GameState& state)
-{
-	List& players = boost::get<List>(boost::get<Map>(state.getVariables())["players"]);
-	for (Variable& player : players) {
-		const std::string& name = boost::get<std::string>(boost::get<Map>(player)["name"]);
-		server.send({state.getConnectionByName(name), value.fill_with(state.getVariables()) });
-	}
-}
+// void GlobalMessageRule::run(Server& server, GameState& state)
+// {
+// 	List& players = boost::get<List>(boost::get<Map>(state.getVariables())["players"]);
+// 	for (Variable& player : players) {
+// 		const std::string& name = boost::get<std::string>(boost::get<Map>(player)["name"]);
+// 		server.send({state.getConnectionByName(name), value.fill_with(state.getVariables()) });
+// 	}
+// }
 
 class MockGlobalMessageRule : public GlobalMessageRule {
 public:
@@ -44,42 +44,45 @@ public:
 	MOCK_METHOD2(run, void(Server& server, GameState& state));
 };
 
-
-
-TEST(MyDumbTest, TestFalse) {
-    EXPECT_FALSE(3*2 == 5);
-    ASSERT_TRUE(true);
-}
+// TEST(MyDumbTest, TestFalse) {
+//     EXPECT_FALSE(3*2 == 5);
+//     ASSERT_TRUE(true);
+// }
 
 TEST(RuleTest, GlobalMessageRuleTest) {
+
+	// initialize environment
+
+	std::vector<Configuration> configurations;
+
 	std::ifstream serverconfig{"../configs/server/testing.json"};
  	if (serverconfig.fail()) {
  		std::cout << "Could not open the rule testing configuration file" << std::endl;
   }
 
 	json serverspec = json::parse(serverconfig);
-	//configurations.reserve(serverspec["games"].size());
+	configurations.reserve(serverspec["games"].size());
 
-	// for ([[maybe_unused]] const auto& [key, gamespecfile]: serverspec["games"].items()) {
-	// 	std::ifstream gamespecstream{std::string(gamespecfile)};
-	// 	if (gamespecstream.fail()) {
-	// 		std::cout << "Could not open the game configuration file " << gamespecfile << std::endl;
-	// 		return 1;
-	// 	}
+	for ([[maybe_unused]] const auto& [key, gamespecfile]: serverspec["games"].items()) {
+		std::ifstream gamespecstream{std::string(gamespecfile)};
+		if (gamespecstream.fail()) {
+			std::cout << "Could not open the game configuration file " << gamespecfile << std::endl;
+		}
     
-	// 	json gamespec = json::parse(gamespecstream);
-	// 	configurations.emplace_back(gamespec);
-	// 	std::cout << "\nTranslated game " << gamespecfile << "\n\n";
-	// }
+		json gamespec = json::parse(gamespecstream);
+		configurations.emplace_back(gamespec);
+		std::cout << "\nTranslated game " << gamespecfile << "\n\n";
+	}
 
   //Server server{port, getHTTPMessage(serverspec["indexhtml"]), onConnect, onDisconnect};
   //PseudoServer pseudoServer{port, getHTTPMessage(serverspec["indexhtml"]), onConnect, onDisconnect};
   //PseudoServer pseudoServer{randPortNum, randStr, onConnect, onDisconnect};
-  PseudoServer pseudoServer;
+	PseudoServer pseudoServer;
+	Server& server = pseudoServer;
 
 
 	EXPECT_FALSE(3*2 == 5);
-    ASSERT_TRUE(true);
+	ASSERT_TRUE(true);
 }
 
 
